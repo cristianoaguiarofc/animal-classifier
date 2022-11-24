6cd33da9ed240808c6dbc4bd9f04d1766ff2ad20
Cristiano Andrade de Aguiar <cristianoandrade.deaguiar@gmail.com>
2022-11-24 00:22:11 -0300

    Adição de animais

--- src/enuns/animalsEnum.js
+++ src/enuns/animalsEnum.js
@@ -1,15 +1,171 @@
 const animalsInformation = {
+    default: {
+        color: '#9ea4db',
+        image: 'img/default.png'
+    },
     bear: {
         color: '#ffc4be',
-        image: 'img/bear.jpg'
+        image: 'img/bear.png'
     },
     cat: {
         color: '#f2a5ad',
-        image: 'img/cat.jpg'
+        image: 'img/cat.png'
+    },
+    swan: {
+        color: '#C6E69B',
+        image: 'img/swan.png'
+    },
+    tuatara: {
+        color: '#FFC4BC',
+        image: 'img/tuatara.png'
+    },
+    hare: {
+        color: '#C6E69B',
+        image: 'img/hare.png'
+    },
+    carp: {
+        color: '#C6E69B',
+        image: 'img/carp.png'
+    },
+    chicken: {
+        color: '#AEDAFF',
+        image: 'img/chicken.png'
+    },
+    clam: {
+        color: '#FFC4BC',
+        image: 'img/clam.png'
+    },
+    crab: {
+        color: '#C6E69B',
+        image: 'img/crab.png'
+    },
+    crow: {
+        color: '#FFC4BC',
+        image: 'img/crow.png'
+    },
+    dogfish: {
+        color: '#FFC4BC',
+        image: 'img/dogfish.png'
+    },
+    dolphin: {
+        color: '#AEDAFF',
+        image: 'img/dolphin.png'
+    },
+    aardvark: {
+        color: '#C6E69B',
+        image: 'img/aardvark.png'
+    },
+    duck: {
+        color: '#FFC4BC',
+        image: 'img/duck.png'
+    },
+    elephant: {
+        color: '#AEDAFF',
+        image: 'img/elephant.png'
+    },
+    flamingo: {
+        color: '#C6E69B',
+        image: 'img/flamingo.png'
+    },
+    flea: {
+        color: '#FFC4BC',
+        image: 'img/flea.png'
+    },
+    frog: {
+        color: '#AEDAFF',
+        image: 'img/frog.png'
+    },
+    fruitbat: {
+        color: '#C6E69B',
+        image: 'img/fruitbat.png'
+    },
+    gnat: {
+        color: '#FFC4BC',
+        image: 'img/gnat.png'
+    },
+    goat: {
+        color: '#AEDAFF',
+        image: 'img/goat.png'
+    },
+    gorilla: {
+        color: '#AEDAFF',
+        image: 'img/gorilla.png'
+    },
+    gull: {
+        color: '#FFC4BC',
+        image: 'img/gull.png'
+    },
+    hamster: {
+        color: '#AEDAFF',
+        image: 'img/hamster.png'
+    },
+    honeybee: {
+        color: '#C6E69B',
+        image: 'img/honeybee.png'
+    },
+    ladybird: {
+        color: '#FFC4BC',
+        image: 'img/ladybird.png'
+    },
+    lark: {
+        color: '#AEDAFF',
+        image: 'img/lark.png'
+    },
+    lion: {
+        color: '#C6E69B',
+        image: 'img/lion.png'
+    },
+    lobster: {
+        color: '#AEDAFF',
+        image: 'img/lobster.png'
+    },
+    octopus: {
+        color: '#FFC4BC',
+        image: 'img/octopus.png'
+    },
+    penguin: {
+        color: '#C6E69B',
+        image: 'img/penguin.png'
+    },
+    scorpion: {
+        color: '#FFC4BC',
+        image: 'img/scorpion.png'
+    },
+    seahorse: {
+        color: '#AEDAFF',
+        image: 'img/seahorse.png'
+    },
+    seal: {
+        color: '#C6E69B',
+        image: 'img/seal.png'
+    },
+    squirrel: {
+        color: '#FFC4BC',
+        image: 'img/squirrel.png'
+    },
+    starfish: {
+        color: '#AEDAFF',
+        image: 'img/starfish.png'
+    },
+    stingray: {
+        color: '#C6E69B',
+        image: 'img/stingray.png'
+    },
+    sealion: {
+        color: '#AEDAFF',
+        image: 'img/sealion.png'
+    },
+    tortoise: {
+        color: '#C6E69B',
+        image: 'img/tortoise.png'
+    },
+    worm: {
+        color: '#FFC4BC',
+        image: 'img/worm.png'
     },
     dog: {
         color: '#9edadb',
-        image: 'img/dog.jpg'
+        image: 'img/dog.png'
     }
 }
 
